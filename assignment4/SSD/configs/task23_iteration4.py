5e299b772defbe9262d4ff12d25bee6480f0661b
Alvin Tan De Jun <>
2022-05-02 19:46:04 +0200

    Add config for iteration 4 (improved weight init)

---
+++ assignment4/SSD/configs/task23_iteration4.py
@@ -0,0 +1,26 @@
+from .task23_iteration3 import (
+    train,
+    optimizer,
+    schedulers,
+    data_train,
+    data_val,
+    train_cpu_transform,
+    val_cpu_transform,
+    gpu_transform,
+    label_map,
+    anchors,
+    backbone,
+    model,
+    loss_objective
+)
+from ssd.modeling import RetinaNet
+from tops.config import LazyCall as L
+
+model = L(RetinaNet)(
+    feature_extractor="${backbone}",
+    anchors="${anchors}",
+    loss_objective="${loss_objective}",
+    num_classes=8 + 1,  # Add 1 for background
+    use_improved_weight=True,
+    use_deeper_heads=True
+)
