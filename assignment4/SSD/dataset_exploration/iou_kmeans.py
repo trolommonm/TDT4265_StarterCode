1823620c04eef64b474e77df5128825636c42ccc
Brandon Yeow <brandonywl@gmail.com>
2022-05-08 01:54:55 +0200

    Refactored out IOU Kmeans

--- assignment4/SSD/dataset_exploration/iou_kmeans.py
+++ assignment4/SSD/dataset_exploration/iou_kmeans.py
@@ -1,76 +1,71 @@
-def iou(boxes, clusters):
-    """ Calculates Intersection over Union between the provided boxes and cluster centroids
-        Input:
-            boxes: Bounding boxes
-            clusters: cluster centroids
-        Output:
-            IoU between boxes and cluster centroids
-    """
-    n = boxes.shape[0]
-    k = np.shape(clusters)[0]
+import numpy as np
 
-    box_area = boxes[:, 0] * boxes[:, 1] # Area = width * height
-    # Repeating the area for every cluster as we need to calculate IoU with every cluster
-    box_area = box_area.repeat(k)
-    box_area = np.reshape(box_area, (n, k))
+def iou(boxes, clusters):
+    n = boxes.shape[0]
+    k = clusters.shape[0]    
+
+    box_area = boxes[:, 0] * boxes[:, 1]
+    box_area = box_area.repeat(k).reshape((-1, k))
 
     cluster_area = clusters[:, 0] * clusters[:, 1]
-    cluster_area = np.tile(cluster_area, [1, n])
-    cluster_area = np.reshape(cluster_area, (n, k))
+    cluster_area = cluster_area.repeat(n).reshape((k,n)).T
 
 
-    box_w_matrix = np.reshape(boxes[:, 0].repeat(k), (n, k))
-    cluster_w_matrix = np.reshape(np.tile(clusters[:, 0], (1, n)), (n, k))
-    min_w_matrix = np.minimum(cluster_w_matrix, box_w_matrix)
+    def get_min_across_axis(boxes, clusters, axis):
+        n = boxes.shape[0] # Number of boxes
+        k = clusters.shape[0] # Number of clusters
+        
+        # Broadcasts the boxes and clusters and returns an (n, k) array where
+        # each row represents the minimum intersection each box has with each cluster (column) on that axis.
 
-    box_h_matrix = np.reshape(boxes[:, 1].repeat(k), (n, k))
-    cluster_h_matrix = np.reshape(np.tile(clusters[:, 1], (1, n)), (n, k))
-    min_h_matrix = np.minimum(cluster_h_matrix, box_h_matrix)
-    inter_area = np.multiply(min_w_matrix, min_h_matrix)
+        boxes = boxes[:,axis]
+        clusters = clusters[:,axis]
+    
+        # Each box axis value is repeated for each cluster
+        box_axis_broadcast_k = boxes.repeat(k).reshape((n, k))
+        # Each cluster axis value is repeated for each box, reshaped and transposed.
+        cluster_axis_broadcast_n = clusters.repeat(n).reshape((k, n)).T
+        return np.minimum(box_axis_broadcast_k, cluster_axis_broadcast_n)
 
-    result = inter_area / (box_area + cluster_area - inter_area)
-    return result
+
+    min_w_similarity = get_min_across_axis(boxes, clusters, 0)
+    min_h_similarity = get_min_across_axis(boxes, clusters, 1)
+    intersection_area = min_w_similarity * min_h_similarity
+
+    # Union = Area Box + Area Cluster - Area Intersection
+    return intersection_area / (box_area + cluster_area - intersection_area)
 
 
 def avg_iou(boxes, clusters):
-    """ Calculates average IoU between the GT boxes and clusters 
-        Input:
-            boxes: array, having width and height of all the GT boxes
-        Output:
-            Returns numpy array of average IoU with all the clusters
-    """
-    return np.mean([np.max(iou(boxes, clusters), axis=1)])
+    highest_iou = np.max(iou(boxes, clusters), axis=1)
+    return np.mean([highest_iou])
 
 
-def iou_kmeans(boxes, k):
-    """ Executes k-means clustering on the rovided width and height of boxes with IoU as
-        distance metric.
-        Input:
-            boxes: numpy array containing with and height of all the boxes
-        Output:
-            clusters after convergence
-    """
+def iou_kmeans(boxes, k, random_state=None):
+    # Implements a standard k-means algorithm with a custom distance metric.
+    
     num_boxes = boxes.shape[0]
-    distances = np.empty((num_boxes, k))
-    last_cluster = np.zeros((num_boxes, ))
 
-    # Initializing the clusters
-    np.random.seed()
-    clusters = boxes[np.random.choice(num_boxes, k, replace=False)]
+    distance = np.zeros((num_boxes, k)) # Create a [num_box, k] array to hold distance for each box to each cluster centroid.
+    last_cluster = np.zeros((num_boxes, )) # Initialize each box to be tagged as cluster 0.
+    init_box_choices = np.random.choice(num_boxes, k, replace=False) # Randomly pick a starting centroid.
+    clusters = boxes[init_box_choices]
 
-    # Optimizarion loop
+    mean_losses = []
+
     while True:
 
-        distances = 1 - iou(boxes, clusters)
-        mean_distance = np.mean(distances)
-        sys.stdout.write('\r>> Mean loss: %f' % (mean_distance))
-        sys.stdout.flush()
+        distance = 1 - iou(boxes, clusters)
+        mean_losses.append(np.mean(distance))
 
-        current_nearest = np.argmin(distances, axis=1)
-        if(last_cluster == current_nearest).all():
-            break # The model is converged
-        for cluster in range(k):
-            clusters[cluster] = np.mean(boxes[current_nearest == cluster], axis=0)
+        current_nearest = np.argmin(distance, axis=1)
+        if (last_cluster == current_nearest).all():
+            break # We are done
+
+        for cluster_idx in range(k):
+            new_boxes_in_cluster = boxes[current_nearest == cluster_idx]
+            clusters[cluster_idx] = np.mean(new_boxes_in_cluster, axis=0)
 
         last_cluster = current_nearest
+        
     return clusters, last_cluster
